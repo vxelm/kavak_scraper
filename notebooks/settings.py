cd18a4f278621d4f7a43d8862a770fa111ea78bf
axguz <axguzvega@gmail.com>
2026-01-27 19:37:23 -0600

    Refactor settings and enrichers: streamline directory paths and enhance logging setup

--- notebooks/settings.py
+++ notebooks/settings.py
@@ -3,15 +3,21 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATA_DIR = BASE_DIR / "data"
-RAW_HTML_DIR = DATA_DIR / "raw" / "html"
-PROCESSED_JSON_DIR = DATA_DIR / "processed" / "json"
-RAW_JSON_DIR = DATA_DIR / "raw" / "json"
+RAW_DATA = BASE_DIR / "raw"
+PROCESSED_DATA = BASE_DIR / "processed"
+
+RAW_HTML_DIR = RAW_DATA / "html"
+RAW_JSON_DIR = RAW_DATA / "json"
+PROCESSED_JSON_DIR = PROCESSED_DATA / "json"
 LOGGING_DIR  = BASE_DIR / "config" / "logs"
+FINANCIAL_DATA_DIR = PROCESSED_DATA / "csv" / "financial_data"
 
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 RAW_HTML_DIR.mkdir(parents=True, exist_ok=True)
 PROCESSED_JSON_DIR.mkdir(parents=True, exist_ok=True)
+RAW_JSON_DIR.mkdir(parents=True, exist_ok=True)
 LOGGING_DIR.mkdir(parents=True, exist_ok=True)
+FINANCIAL_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 BASE_URL = "https://www.kavak.com/mx/seminuevos"
 MAX_WORKERS = 3
