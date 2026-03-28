055c804cfbe717c93c39f16063ed19599d467828
axguz <axguzvega@gmail.com>
2026-03-28 12:13:47 -0600

    Feat(schemas): add PlanFinanciero and Auto classes for financial plan representation and establish relationships

--- src/schemas.py
+++ src/schemas.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, Session, SQLModel, create_engine
+from sqlmodel import Field, Session, SQLModel, create_engine, Relationship
 from pydantic import BaseModel, Field
 from typing import Optional
 from datetime import datetime
@@ -21,7 +21,29 @@ class Autokavak(BaseModel):
 
 
 
-class Autokavak_table(SQLModel, table=True):
+class PlanFinanciero(SQLModel, table=True):
+    #PK autoincremental
+    id: int | None = Field(default=None, primary_key=True)
+    
+    #Llave foranea apuntando a tabla 'auto'
+    id_auto: str = Field(foreign_key="auto.id")
+    
+    #Datos del negocio
+    precio: int
+    tasa_servicio: float
+    plazo: int
+    mensualidad: int
+    tasa_interes: float
+    seguro: float
+    enganche_simulado: float
+    enganche_min: float
+    enganche_max: float
+
+    #Relacion el 'auto' al que pertenece este plan
+    auto: "Auto" = Relationship(back_populates="planes")
+
+
+class Auto(SQLModel, table=True):
     #NO nulos NO Nones
     id: str = Field(primary_key=True)
     slug: str
@@ -33,6 +55,9 @@ class Autokavak_table(SQLModel, table=True):
     km: int 
 
     #Datos que pueden ser nulos o vacios
-    gear: str | None
+    gear: str | None = None
     discount_offer: int = Field(default=0)
     details: Optional[str] | None = None
+
+    #Lista de planes financieros que pertenecen al auto
+    planes: list[PlanFinanciero] = Relationship(back_populates="auto")
