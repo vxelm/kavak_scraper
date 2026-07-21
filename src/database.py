e4fb9b6548596dd359140cc3766f97f8ea3ea907
axguz <axguzvega@gmail.com>
2026-07-21 12:02:03 -0600

    feat(database): refactor database connection to use environment variables for PostgreSQL

--- src/database.py
+++ src/database.py
@@ -1,8 +1,14 @@
 from sqlmodel import SQLModel, create_engine
-from src.settings import DATABASE_URL
+from src.settings import build_url_db
+from src.models import Auto, FinancialPlan
 
-#echo=False NO imprimir en consola.
-engine = create_engine(DATABASE_URL, echo=False)
+_engine = None
+def get_engine():
+    global _engine
+    if _engine is None:
+        _engine = create_engine(build_url_db(), echo=False) #echo=False NO imprimir en consola.
+
+    return _engine
 
 def create_db_n_tables():
-    SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(get_engine())
